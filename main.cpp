452f0061680b336419366bde59a76d0e1b2f33b5
archibate <1931127624@qq.com>
2022-01-15 13:47:42 +0800

    add sqrtdot

--- main.cpp
+++ main.cpp
@@ -1,6 +1,87 @@
-#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+#include <numeric>
+#include <algorithm>
+#include "ticktock.h"
+
+// TODO: 并行化所有这些 for 循环
+
+template <class T, class Func>
+std::vector<T> fill(std::vector<T> &arr, Func const &func) {
+    TICK(fill);
+    for (size_t i = 0; i < arr.size(); i++) {
+        arr[i] = func(i);
+    }
+    TOCK(fill);
+    return arr;
+}
+
+template <class T>
+void saxpy(T a, std::vector<T> &x, std::vector<T> const &y) {
+    TICK(saxpy);
+    for (size_t i = 0; i < x.size(); i++) {
+       x[i] = a * x[i] + y[i];
+    }
+    TOCK(saxpy);
+}
+
+template <class T>
+T sqrtdot(std::vector<T> const &x, std::vector<T> const &y) {
+    TICK(sqrtdot);
+    T ret = 0;
+    for (size_t i = 0; i < std::min(x.size(), y.size()); i++) {
+        ret += x[i] * y[i];
+    }
+    ret = std::sqrt(ret);
+    TOCK(sqrtdot);
+    return ret;
+}
+
+template <class T>
+T minvalue(std::vector<T> const &x) {
+    TICK(minvalue);
+    T ret = x[0];
+    for (size_t i = 1; i < x.size(); i++) {
+        if (x[i] < ret)
+            ret = x[i];
+    }
+    TOCK(minvalue);
+    return ret;
+}
+
+template <class T>
+std::vector<T> magicfilter(std::vector<T> const &x, std::vector<T> const &y) {
+    TICK(magicfilter);
+    std::vector<T> res;
+    for (size_t i = 0; i < std::min(x.size(), y.size()); i++) {
+        if (x[i] > y[i]) {
+            res.push_back(x[i]);
+        } else if (y[i] > x[i] && y[i] > 0.5f) {
+            res.push_back(y[i]);
+            res.push_back(x[i] * y[i]);
+        }
+    }
+    TOCK(magicfilter);
+    return res;
+}
 
 int main() {
-    printf("Hello, world!\n");
+    size_t n = 1<<26;
+    std::vector<float> x(n);
+    std::vector<float> y(n);
+
+    fill(x, [&] (size_t i) { return std::sin(i); });
+    fill(y, [&] (size_t i) { return std::cos(i); });
+
+    saxpy(0.5f, x, y);
+
+    std::cout << sqrtdot(x, y) << std::endl;
+    std::cout << minvalue(x) << std::endl;
+
+    auto arr = magicfilter(x, y);
+    std::cout << arr.size() << std::endl;
+
     return 0;
 }
